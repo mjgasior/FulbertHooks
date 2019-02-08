59d6522c5e37b6f01039403f89ea25c55fead188
Michał J. Gąsior <michaljgasior@gmail.com>
2019-02-08 21:27:30 +0100

    Menu

--- src/StarWars/StarWars.js
+++ src/StarWars/StarWars.js
@@ -1,14 +1,23 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
 import "./StarWars.css";
 
-export const StarWars = () => {
-  return (
-    <>
-      <div className="column left">
-        <Link to="/">Return to main</Link>
+export class StarWars extends Component {
+  handleClick = () => {
+    const wrapper = document.getElementById("wrapper");
+    wrapper.classList.toggle("is-nav-open");
+  };
+
+  render() {
+    return (
+      <div id="wrapper" className="wrapper">
+        <div className="nav">
+          <button className="nav__icon" onClick={this.handleClick}>Open</button>
+          <div className="nav__body">Lorem dipsum</div>
+        </div>
+        <div className="content">
+          Here is some content
+        </div>
       </div>
-      <div className="column right">b</div>
-    </>
-  );
-};
+    );
+  }
+}
